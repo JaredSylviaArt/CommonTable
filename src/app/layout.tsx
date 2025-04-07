e27f648a10b03d551fdba5eff58be5b44eb3e0e6
JaredSylviaArt <jaredted@gmail.com>
2025-04-07 09:46:52 -0400

    Fix Geist font imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { GeistSans } from 'geist/font/sans'
+import { GeistMono } from 'geist/font/mono'
+import './globals.css'
 import Navigation from "@/components/Navigation";
 import Sidebar from "@/components/Sidebar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function RootLayout({
   children,
@@ -19,6 +10,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable} antialiased bg-gray-50`}>
+      <body className="min-h-screen flex flex-col">
+        <Navigation />
+        
+        {/* Main Content Area */}
+        <div className="flex flex-1">
+          <Sidebar />
+          
+          {/* Main Content */}
+          <main className="flex-1 bg-white">
+            {children}
+          </main>
+        </div>
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50`}>
         <div className="min-h-screen flex flex-col">
