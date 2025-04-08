40bc4d95e809dd2e87d42bff21098c375f95cce6
JaredSylviaArt <jaredted@gmail.com>
2025-04-08 05:39:17 -0400

    Configure for Netlify deployment

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/CommonTable',
   images: {
     unoptimized: true,
   },
-  // Remove any other conflicting options like swcMinify
 }
 
 module.exports = nextConfig 
